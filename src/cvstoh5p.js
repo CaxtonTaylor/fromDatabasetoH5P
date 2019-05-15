1c93c6fe3d86456ec105ee3eed2c7664517940ba
Andrés Corredor <andrescorredor20@gmail.com>
2019-05-15 11:29:46 -0500

    abstraction cvs

---
+++ src/cvstoh5p.js
@@ -0,0 +1,84 @@
+'use strict'
+const Utils = require("../lib/utils");
+const utils = new Utils();
+
+module.exports =
+    class CVStoH5P {
+        constructor(Lesson) {
+            this.Lesson = Lesson;
+            this.listH5Ps = [];
+        }
+        async init() {
+            this.Lesson = await utils.readJSON(this.jsonPATH);
+        }
+        async makeH5Ps() {
+            await this.makeFolders();
+            await this.makeSubFolders();
+            await this.loadJSONs();
+            await this.loadCVS();
+            await this.executeCVS();
+            await this.updateJSONs();
+            await this.zipH5Ps();
+        }
+        async makeFolders() {
+            let relativePath = 'English/General/' + this.Lesson.Level + '/' + this.Lesson.Level + '/Lesson' + this.Lesson.name + '/' + this.nameFolder
+            this.ProcessPath = './H5Pprocess/' + relativePath
+            this.UPPath = './UP/' + relativePath
+            await utils.emptyDir(this.ProcessPath)
+            await utils.emptyDir(this.UPPath)
+            await utils.createDir(this.UPPath)
+        }
+        async makeSubFolders() {
+            let H5PTestPath = './H5Ps/' + this.H5Plibrary
+            await this.updateforeachH5P(async function (h5p, h5p_name) {
+                h5p.name = h5p_name
+                h5p.path = this.ProcessPath + h5p_name + this.subname;
+                await utils.copyDir(H5PTestPath, h5p.path);
+                return h5p;
+            }.bind(this))
+        }
+        async loadJSONs() {
+            await this.updateforeachH5P(async function (h5p) {
+                h5p.h5p = await utils.readJSON(h5p.path + 'h5p.json');
+                h5p.content = await utils.readJSON(h5p.path + 'content/content.json');
+                return h5p;
+            }.bind(this))
+        }
+        async loadCVS() {
+            let cvs = await utils.readCVS('./CSV/' + this.cvs_name);
+            cvs.shift()
+            cvs.shift();//remove two rows titles
+            this.cvs = cvs;
+        }
+        async updateJSONs() {
+            await this.updateforeachH5P(async function (h5p) {
+                await utils.writeJSON(h5p.path + 'h5p.json', h5p.h5p);
+                await utils.writeJSON(h5p.path + 'content/content.json', h5p.content);
+                return h5p;
+            }.bind(this))
+        }
+        async zipH5Ps() {
+            await this.updateforeachH5P(async function (h5p) {
+                let h5p_name = this.Lesson.Level + 'Lesson' + this.Lesson.name + h5p.name + this.subh5pname + '.h5p';
+                await utils.exec('cd ' + h5p.path + ' && zip -r ' + h5p_name + ' .');
+                await utils.exec('mv ' + h5p.path + h5p_name + ' ' + this.UPPath + h5p_name, true);
+
+                return h5p;
+            }.bind(this))
+        }
+        async executeCVS() {
+            for (let i = 0; i < this.cvs.length; i++) {
+                let row = this.cvs[i];
+                await this.processRow(row);
+            }
+        }
+        async updateforeachH5P(funct) {
+            for (const h5p_name in this.listH5Ps) {
+                if (this.listH5Ps.hasOwnProperty(h5p_name)) {
+                    let h5p = this.listH5Ps[h5p_name];
+                    h5p = await funct(h5p, h5p_name, this.listH5Ps)
+                    this.listH5Ps[h5p_name] = h5p
+                }
+            }
+        }
+    }
--- src/vocabularies.js
+++ src/vocabularies.js
@@ -1,85 +1,39 @@
 'use strict'
 const Utils = require("../lib/utils");
+const CVStoH5P = require("./cvstoh5p");
 const utils = new Utils();
 module.exports =
-    class Vocabularies {
+    class Vocabularies extends CVStoH5P {
+
         constructor(Lesson) {
-            this.Lesson = Lesson;
-            this.list = [];
-            this.list['Listening'] = {
+            super(Lesson);
+            this.nameFolder = 'Vocabularies/'
+            this.H5Plibrary = 'VocabularyTest/'
+            this.subname = '_VocabularyTest/'
+            this.cvs_name = 'VocabularyTest.csv'
+            this.subh5pname = 'Vocabulary'
+            this.listH5Ps['Listening'] = {
                 name: 'Listening',
                 init: 3
 
             }
-            this.list['Writing'] = {
+            this.listH5Ps['Writing'] = {
                 name: 'Writing',
                 init: 7
             }
-            this.list['Reading'] = {
+            this.listH5Ps['Reading'] = {
                 name: 'Reading',
                 init: 11
             }
-            this.list['Monologue'] = {
+            this.listH5Ps['Monologue'] = {
                 name: 'Monologue',
                 init: 15
             }
-            this.list['Conversation'] = {
+            this.listH5Ps['Conversation'] = {
                 name: 'Conversation',
                 init: 19
             }
         }
-        async makeH5Ps() {
-            await this.makeFolders();
-            await this.loadJSONs();
-            await this.loadCVS();
-            await this.executeCVS();
-            await this.updateJSONs();
-            await this.zipH5Ps();
-        }
-
-        async makeFolders() {
-            let relativePath = 'English/General/' + this.Lesson.Level + '/' + this.Lesson.Level + '/Lesson' + this.Lesson.name + '/'
-            this.ProcessPath = './H5Pprocess/' + relativePath
-            this.UPPath = './UP/' + relativePath
-            let VocabularyTestPath = './H5Ps/VocabularyTest/'
-
-            await utils.emptyDir(this.ProcessPath)
-            await utils.emptyDir(this.UPPath)
-            await utils.createDir(this.UPPath)
-            await this.updateforeachVocabularies(async function (vocabulary, vocabulary_name) {
-                vocabulary.name = vocabulary_name
-                vocabulary.path = this.ProcessPath + vocabulary_name + '_VocabularyTest/'
-                await utils.copyDir(VocabularyTestPath, vocabulary.path);
-
-                return vocabulary;
-            }.bind(this))
-        }
-        async loadJSONs() {
-            await this.updateforeachVocabularies(async function (vocabulary, vocabulary_name) {
-                vocabulary.h5p = await utils.readJSON(vocabulary.path + 'h5p.json');
-                vocabulary.content = await utils.readJSON(vocabulary.path + 'content/content.json');
-                return vocabulary;
-            }.bind(this))
-
-        }
-        async loadCVS() {
-            let cvs = await utils.readCVS('./CSV/VocabularyTest.csv');
-            cvs.shift()
-            cvs.shift();//remove two rows titles
-            this.cvs = cvs;
-        }
-        async vocabularyNamefromrowindex(index) {
-            let name;
-            await this.updateforeachVocabularies(async function (vocabulary, vocabulary_name) {
-
-                if (index >= vocabulary.init) {
-                    name = vocabulary.name;
-                }
-                return vocabulary;
-            }.bind(this))
-            return name
-        }
-
         async processRow(row) {
             const _ = require('lodash');
             for (let i = 1; i < row.length; i++) {
@@ -92,7 +46,7 @@ module.exports =
                     switch (mod) {
                         case 0://h5p update
                             value = row[i + 2];
-                            this.list[vocabularyName].h5p[key] = eval(value)
+                            this.listH5Ps[vocabularyName].h5p[key] = eval(value)
                             break;
                         case 1://content update
                             value = eval('this.' + row[0])
@@ -100,8 +54,8 @@ module.exports =
                                 if (value) {
                                 }
                             } else {
-                                _.set(this.list[vocabularyName].content, key, value)
-                                //console.log(_.get(this.list[vocabularyName].content,key))
+                                _.set(this.listH5Ps[vocabularyName].content, key, value)
+                                //console.log(_.get(this.listH5Ps[vocabularyName].content,key))
                             }
                             return 'content'//check if exist 3 if exist move accest and update name
                             break;
@@ -113,36 +67,16 @@ module.exports =
                 }
             }
         }
-        async executeCVS() {
-            for (let i = 0; i < this.cvs.length; i++) {
-                let row = this.cvs[i];
-                await this.processRow(row);
-            }
-        }
-        async updateJSONs() {
-            await this.updateforeachVocabularies(async function (vocabulary) {
-                await utils.writeJSON(vocabulary.path + 'h5p.json', vocabulary.h5p);
-                await utils.writeJSON(vocabulary.path + 'content/content.json', vocabulary.content);
-                return vocabulary;
-            }.bind(this))
-        }
-        async zipH5Ps() {
-            await this.updateforeachVocabularies(async function (vocabulary) {
-                let h5p_name = this.Lesson.Level + 'Lesson' + this.Lesson.name + vocabulary.name + 'Vocabulary.h5p';
-                await utils.exec('cd ' + vocabulary.path + ' && zip -r ' + h5p_name + ' .');
-                await utils.exec('mv ' + vocabulary.path + h5p_name + ' ' + this.UPPath + h5p_name, true);
+        async vocabularyNamefromrowindex(index) {
+            let name;
+            await this.updateforeachH5P(async function (vocabulary, vocabulary_name) {
 
+                if (index >= vocabulary.init) {
+                    name = vocabulary.name;
+                }
                 return vocabulary;
             }.bind(this))
-        }
-        async updateforeachVocabularies(funct) {
-            for (const vocabulary_name in this.list) {
-                if (this.list.hasOwnProperty(vocabulary_name)) {
-                    let vocabulary = this.list[vocabulary_name];
-                    vocabulary = await funct(vocabulary, vocabulary_name, this.list)
-                    this.list[vocabulary_name] = vocabulary
-                }
-            }
+            return name
         }
 
     }
