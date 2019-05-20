1e7763b7060f9b5a5aebcacfe777ae7a9fde97db
Andrés Corredor <andrescorredor20@gmail.com>
2019-05-20 10:20:13 -0500

    ReadAndWrite

---
+++ src/ReadAndWrite.js
@@ -0,0 +1,21 @@
+'use strict'
+const Utils = require("../lib/utils");
+const utils = new Utils();
+const CVStoH5P = require("./cvstoh5p");
+module.exports =
+    class ReadAndWrite extends CVStoH5P {
+
+        constructor(Lesson) {
+            super(Lesson);
+            this.nameFolder = 'ReadAndWrite/'
+            this.H5Plibrary = 'ReadAndWrite/'
+            this.subname = '/'
+            this.cvs_name = 'ReadAndWrite.csv'
+            this.subh5pname = 'ReadAndWrite'
+            this.listH5Ps['ReadAndWrite'] = {
+                name: 'ReadAndWrite',
+                init: 1
+            }
+        }
+
+    }
