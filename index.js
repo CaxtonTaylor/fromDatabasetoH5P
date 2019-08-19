27740530bdc93dcae35a8931753e8a1412684e4f
Andmat7 <andrescorredor20@gmail.com>
2019-08-19 13:33:12 -0500

    new packages

--- index.js
+++ index.js
@@ -14,21 +14,23 @@ async function index(){
     // }
 
     await h5p.processJSON('./DATABASE/English/General/A1/Lesson1/GeneralEnglishA1Lesson1.json')
-    // await h5p.processJSON('./DATABASE/English/General/Basic/Lesson1/GeneralEnglishBasicLesson1.json')
-    // await h5p.processJSON('./DATABASE/English/General/Basic/Lesson2/GeneralEnglishBasicLesson2.json')
-    // await h5p.processJSON('./DATABASE/English/General/Basic/Lesson3/GeneralEnglishBasicLesson3.json')
+    // await h5p.processJSON('./DATABASE/English/General/B1_3/Lesson20/GeneralEnglishB1_3Lesson20.json')
+
+    await h5p.processJSON('./DATABASE/English/General/Basic/Lesson1/GeneralEnglishBasicLesson1.json')
+    await h5p.processJSON('./DATABASE/English/General/Basic/Lesson2/GeneralEnglishBasicLesson2.json')
+    await h5p.processJSON('./DATABASE/English/General/Basic/Lesson3/GeneralEnglishBasicLesson3.json')
    
-    // await h5p.processJSON('./DATABASE/English/General/Elementary/Lesson1/GeneralEnglishElementaryLesson1.json')
-    // await h5p.processJSON('./DATABASE/English/General/Elementary/Lesson2/GeneralEnglishElementaryLesson2.json')
-    // await h5p.processJSON('./DATABASE/English/General/Elementary/Lesson3/GeneralEnglishElementaryLesson3.json')
+    await h5p.processJSON('./DATABASE/English/General/Elementary/Lesson1/GeneralEnglishElementaryLesson1.json')
+    await h5p.processJSON('./DATABASE/English/General/Elementary/Lesson2/GeneralEnglishElementaryLesson2.json')
+    await h5p.processJSON('./DATABASE/English/General/Elementary/Lesson3/GeneralEnglishElementaryLesson3.json')
     
-    // await h5p.processJSON('./DATABASE/English/General/Waystage/Lesson1/GeneralEnglishWaystageLesson1.json')
-    // await h5p.processJSON('./DATABASE/English/General/Waystage/Lesson2/GeneralEnglishWaystageLesson2.json')
-    // await h5p.processJSON('./DATABASE/English/General/Waystage/Lesson3/GeneralEnglishWaystageLesson3.json')
+    await h5p.processJSON('./DATABASE/English/General/Waystage/Lesson1/GeneralEnglishWaystageLesson1.json')
+    await h5p.processJSON('./DATABASE/English/General/Waystage/Lesson2/GeneralEnglishWaystageLesson2.json')
+    await h5p.processJSON('./DATABASE/English/General/Waystage/Lesson3/GeneralEnglishWaystageLesson3.json')
     
-    // await h5p.processJSON('./DATABASE/English/General/Lower-Intermediate/Lesson1/GeneralEnglishLower-IntermediateLesson1.json')
-    // await h5p.processJSON('./DATABASE/English/General/Lower-Intermediate/Lesson3/GeneralEnglishLower-IntermediateLesson3.json')
-    // await h5p.processJSON('./DATABASE/English/General/Lower-Intermediate/Lesson2/GeneralEnglishLower-IntermediateLesson2.json')
+    await h5p.processJSON('./DATABASE/English/General/Lower-Intermediate/Lesson1/GeneralEnglishLower-IntermediateLesson1.json')
+    await h5p.processJSON('./DATABASE/English/General/Lower-Intermediate/Lesson3/GeneralEnglishLower-IntermediateLesson3.json')
+    await h5p.processJSON('./DATABASE/English/General/Lower-Intermediate/Lesson2/GeneralEnglishLower-IntermediateLesson2.json')
     console.log('finish')
 }
 index();
